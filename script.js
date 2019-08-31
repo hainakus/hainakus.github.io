bb7c1b3c8e2377ddb4e033fa50732f6280837e0d
rui <ruitiagomendes@gmail.com>
2019-08-31 23:59:11 +0100

    Initial commit

--- script.js
+++ script.js
@@ -25,29 +25,5 @@ $('.uk-list > li').on('click', function () {
 var player = new Clappr.Player({
     source: "rtmp://95.179.238.114/live/hainaku",
     parentId: "#player",
-    plugins: { 'playback': [RTMP] },
-    rtmpConfig: {
-        swfPath: 'dist/assets/RTMP.swf',
-        scaling: 'stretch',
-        playbackType: 'live',
-        bufferTime: 1,
-        startLevel: 0,
-        switchRules: {
-            "SufficientBandwidthRule": {
-                "bandwidthSafetyMultiple": 1.15,
-                "minDroppedFps": 2
-            },
-            "InsufficientBufferRule": {
-                "minBufferLength": 2
-            },
-            "DroppedFramesRule": {
-                "downSwitchByOne": 10,
-                "downSwitchByTwo": 20,
-                "downSwitchToZero": 24
-            },
-            "InsufficientBandwidthRule": {
-                "bitrateMultiplier": 1.15
-            }
-        }
-    }
+    plugins: { 'playback': [RTMP] }
 });
